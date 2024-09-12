babde06a95ed5192545d37e2703733f8598be8d8
ruiiary <guidews39@gmail.com>
2024-09-12 22:27:05 +0900

    feat: header, footer 연결

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,12 +6,10 @@ import logo from '../assets/LogoTitle.svg'
 const Footer = () => {
   return (
     <S.FooterContainer>
-          <S.FooterMenu src={logo} alt="로고" />
-        <div>
-          <p style={{ fontSize: '1.05em' }}>
-            footer 어쩌구
-          </p>
-        </div>
+      <S.FooterMenu src={logo} alt="로고" />
+      <div>
+        <p style={{ fontSize: '1.05em' }}>footer 어쩌구</p>
+      </div>
     </S.FooterContainer>
   )
 }
