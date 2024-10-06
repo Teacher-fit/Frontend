943132dedebd9e7d89197e17d69db0a822966689
ruiiary <guidews39@gmail.com>
2024-10-06 12:34:28 +0900

    style: 정렬 에러 fix2

--- src/pages/MyFitResult.tsx
+++ src/pages/MyFitResult.tsx
@@ -6,6 +6,7 @@ import remarkGfm from 'remark-gfm'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 import Header from '../components/Header'
+import Menu from '../components/Menu'
 
 import ChatLogo from '../assets/ChatLogo.svg'
 import ReloadIcon from '../assets/ReloadIcon.svg'
@@ -27,20 +28,7 @@ const MyFitResult = () => {
   return (
     <Root>
       <Header />
-      <Menu>
-        <MenuCategory
-          isActive={activeButton === 'home'}
-          onClick={() => handleButtonClick('home')}
-        >
-          HOME
-        </MenuCategory>
-        <MenuCategory
-          isActive={activeButton === 'fit'}
-          onClick={() => handleButtonClick('fit')}
-        >
-          나만의 <Fit>Fit</Fit> 찾기
-        </MenuCategory>
-      </Menu>
+      <Menu />
       <AnswerBox>
         <AnswerInfo>
           <img src={ChatLogo} />
@@ -81,56 +69,6 @@ export const Root = styled.div`
   max-width: 1100px;
   margin: 0 auto;
 `
-export const Menu = styled.div`
-  display: flex; /* inline-flex를 flex로 변경 */
-  padding: 6px;
-  align-items: flex-start;
-  text-align: center;
-  gap: 6px;
-  margin: 0;
-  margin-bottom: 51px;
-  margin-top: 50px;
-  width: 35%;
-  font-size: 16px;
-
-  border-radius: 8px;
-  border: var(--sds-size-stroke-border) solid #d3d5ff;
-  opacity: var(--sds-size-stroke-border);
-  background: #f7f7ff;
-`
-
-export const MenuCategory = styled.button<{ isActive: boolean }>`
-  flex-grow: 1; /* 각 버튼이 동일한 너비를 가짐 */
-  background-color: ${({ isActive }) => (isActive ? '#D3D5FF' : '#ffffff')}; !important
-  font-size: 15px;
-  color: ${({ isActive }) => (isActive ? '#666' : '#000000')};
-  border: none;
-  border-radius: 4px;
-  padding: 8px 20px;
-  white-space: nowrap;
-  font-weight: bold;
-  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.25);
-  appearance: none;
-  transition: all 0.3s ease;
-  width: auto;
-  
-  &:hover {
-    background-color: #f0f0f0;
-    color: #000000;
-    border: 0;
-    outline: none;
-  }
-
-  &:focus {
-    outline: none; 
-    border: none;   
-  }
-`
-
-export const Fit = styled.span`
-  color: #4049f4; /* 파란색으로 강조 */
-  font-weight: bold;
-`
 
 export const AnswerBox = styled.div`
   width: 100%;
@@ -157,40 +95,40 @@ export const AnswerInfo = styled.div`
 
 export const MarkdownWrapper = styled.div`
   margin-top: 30px;
-  margin-left: 30px;
-  margin-right: 30px;
-
-  /* 표 테두리 스타일 추가 */
-  export const MarkdownWrapper = styled.div
-  margin-top: 30px;
-  margin-left: 30px;
+  min-height: 100px;
 
   /* Heading 태그 앞에 마진 추가 */
-  h1, h2, h3, h4, h5, h6 {
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
     margin-top: 1em; /* 각 단락이 Heading으로 구분될 수 있도록 */
   }
 
   /* table의 데이터 가운데 정렬 */
   table {
-    width: 100%; 
+    width: 100%;
     border-collapse: collapse;
     text-align: center; /* 테이블 안의 데이터 가운데 정렬 */
     margin-top: 0.4em;
   }
 
-  th, td {
+  th,
+  td {
     border: 0.3px solid lightgray;
     padding: 8px;
   }
 
   /* 첫 번째 열 전체의 배경색 변경 */
   thead {
-    background-color: #EAEAF8;
+    background-color: #eaeaf8;
   }
 
   /* H2 태그의 크기를 H3 크기로 줄임 */
   h2 {
-    font-size: 1.17em; 
+    font-size: 1.17em;
     margin-bottom: 0.1em;
   }
 
